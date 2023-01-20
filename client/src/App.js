eb3bd60a2ea897512dd05032a9e7e6fdedc3f59a
Kingkarzy <43390918+Kingkarzy@users.noreply.github.com>
2023-01-20 18:57:18 +0100

    another

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material"; 
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
-
 
 function App() {
-  const isAuth = Boolean(useSelector((state) => state.token)); 
   return (
     <div className="App">
      
