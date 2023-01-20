e637337bb833a7a685e1ee35ffb5710b1a9c138c
ciijay99 <chigozieahanwa99@gmail.com>
2023-01-20 18:06:19 +0100

    commit

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,17 +2,11 @@ import { useState } from 'react';
 import {
   Box,
   IconButton,
-  InputBase,
   Typography,
-  Select,
-  MenuItem,
-  FormControl,
   useTheme,
   useMediaQuery,
 } from '@mui/material';
 import {
-  Search,
-  Message,
   DarkMode,
   LightMode,
   Notifications,
@@ -20,25 +14,21 @@ import {
   Menu,
   Close,
 } from '@mui/icons-material';
-import { useDispatch, useSelector } from 'react-redux';
-import { setMode, setLogout } from 'state';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery('(min-width: 1024px)');
 
   const theme = useTheme();
-  const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
-
-  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <Box
@@ -72,24 +62,6 @@ const Navbar = () => {
         >
           Bolt Parcel
         </Typography>
-        {isNonMobileScreens && (
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-            backgroundColor={neutralLight}
-            borderRadius='9px'
-            gap='3rem'
-            padding='0.1rem 1.5rem'
-          >
-            <InputBase placeholder='Search...' />
-            <IconButton>
-              <Search />
-            </IconButton>
-          </Box>
-        )}
       </Box>
 
       {/* DESKTOP NAV */}
@@ -102,6 +74,62 @@ const Navbar = () => {
           }}
           gap='2rem'
         >
+          <Typography
+            fontWeight='bold'
+            // color='primary'
+            onClick={() => navigate('/')}
+            sx={{
+              '&:hover': {
+                color: primaryLight,
+                cursor: 'pointer',
+                fontSize: '25px',
+              },
+            }}
+          >
+            About
+          </Typography>
+          <Typography
+            fontWeight='bold'
+            // color='primary'
+            onClick={() => navigate('/')}
+            sx={{
+              '&:hover': {
+                color: primaryLight,
+                cursor: 'pointer',
+                fontSize: '25px',
+              },
+            }}
+          >
+            Contact
+          </Typography>
+          <Typography
+            fontWeight='bold'
+            // color='primary'
+            onClick={() => navigate('/')}
+            sx={{
+              '&:hover': {
+                color: primaryLight,
+                cursor: 'pointer',
+                fontSize: '25px',
+              },
+            }}
+          >
+            Services
+          </Typography>
+          <Typography
+            fontWeight='bold'
+            // color='primary'
+            onClick={() => navigate('/')}
+            sx={{
+              '&:hover': {
+                color: primaryLight,
+                cursor: 'pointer',
+                fontSize: '25px',
+              },
+            }}
+          >
+            Track
+          </Typography>
           <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === 'dark' ? (
               <DarkMode sx={{ fontSize: '25px' }} />
@@ -109,36 +137,6 @@ const Navbar = () => {
               <LightMode sx={{ color: dark, fontSize: '25px' }} />
             )}
           </IconButton>
-          <Message sx={{ fontSize: '25px' }} />
-          <Notifications sx={{ fontSize: '25px' }} />
-          <Help sx={{ fontSize: '25px' }} />
-          <FormControl
-            variant='standard'
-            value='Ed Roh'
-          >
-            <Select
-              value={fullName}
-              sx={{
-                backgroundColor: neutralLight,
-                width: '150px',
-                borderRadius: '0.25rem',
-                p: '0.25rem 1rem',
-                '& .MuiSvgIcon-root': {
-                  pr: '0.25rem',
-                  width: '3rem',
-                },
-                '& .MuiSelect-select:focus': {
-                  backgroundColor: neutralLight,
-                },
-              }}
-              input={<InputBase />}
-            >
-              <MenuItem>
-                <Typography>{fullName}</Typography>
-              </MenuItem>
-              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
-            </Select>
-          </FormControl>
         </Box>
       ) : (
         <IconButton
@@ -196,38 +194,8 @@ const Navbar = () => {
                 <LightMode sx={{ color: dark, fontSize: '25px' }} />
               )}
             </IconButton>
-            <Message sx={{ fontSize: '25px' }} />
             <Notifications sx={{ fontSize: '25px' }} />
             <Help sx={{ fontSize: '25px' }} />
-            <FormControl
-              variant='standard'
-              value='Ed Roh'
-            >
-              <Select
-                value={fullName}
-                sx={{
-                  backgroundColor: neutralLight,
-                  width: '150px',
-                  borderRadius: '0.25rem',
-                  p: '0.25rem 1rem',
-                  '& .MuiSvgIcon-root': {
-                    pr: '0.25rem',
-                    width: '3rem',
-                  },
-                  '& .MuiSelect-select:focus': {
-                    backgroundColor: neutralLight,
-                  },
-                }}
-                input={<InputBase />}
-              >
-                <MenuItem>
-                  <Typography>{fullName}</Typography>
-                </MenuItem>
-                <MenuItem onClick={() => dispatch(setLogout())}>
-                  Log Out
-                </MenuItem>
-              </Select>
-            </FormControl>
           </Box>
         </Box>
       )}
---
+++ client/src/scenes/Services/Services.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <section className='ser-btm py-md-5'>
+        <div className='container py-4 mt-2'>
+          <h3 className='tittle-w3ls text-center mb-3'>Services</h3>
+          <p className='tit text-center mx-auto'>
+            Services and technology for managing your shipping process – from
+            individuals to enterprises, local to global.
+          </p>
+          <div className='row banner-bottom-main pt-4 mt-md-4'>
+            <div className='col-md-4 banner-grid2'>
+              <div className='banner-subg1'>
+                <span
+                  className='fa fa-globe'
+                  aria-hidden='true'
+                ></span>
+                <h3 className='mt-3'>WAREHOUSING & DISTRIBUTION</h3>
+                <p className='mt-3 mb-3'>
+                  Moving products from manufacturing plants to warehouses,
+                  between international, national, and regional facilities and
+                  to distributors, can represent more than half of your total
+                  logistics costs.
+                </p>
+              </div>
+            </div>
+            <div className='col-md-4 banner-grid2'>
+              <div className='banner-subg1'>
+                <span
+                  className='fa fa-user'
+                  aria-hidden='true'
+                ></span>
+                <h3 className='mt-3'>FREIGHT TRANSPORTATION</h3>
+                <p className='mt-3 mb-3'>
+                  A logistics partner big enough to deliver freight of any kind,
+                  to any place via air, ocean, road or rail, but capable of
+                  giving its customers the personal attention they need. Welcome
+                  to United Parcel Courier.
+                </p>
+                <div className='read-btn'></div>
+              </div>
+            </div>
+            <div className='col-md-4 banner-grid2'>
+              <div className='banner-subg1'>
+                <span
+                  className='fa fa-globe'
+                  aria-hidden='true'
+                ></span>
+                <h3 className='mt-3'>WORLDWIDE TRANSPORT</h3>
+                <p className='mt-3 mb-3'>
+                  Whether we're managing your carriers, creating a dedicated
+                  fleet or a mix of the two, we deliver high levels of service,
+                  reliability and control over your product movement to your
+                  customers.
+                </p>
+                <div className='read-btn'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='wthree-row w3-about py-md-5'>
+        <div className='container py-4 mt-2'>
+          <h3 className='tittle-w3ls text-center mb-3'>Smooth Shipping.</h3>
+          <div className='card-deck pt-4 mt-md-4'>
+            <div className='card'>
+              <img
+                src='images/g1.jpg'
+                className='img-fluid'
+                alt='Card'
+              />
+              <div className='card-body w3ls-card'>
+                <h5 className='card-title'>WAREHOUSE & STORAGE</h5>
+                <p className='card-text mb-3 '>
+                  By understanding your issues and anticipating your business
+                  and logistics needs, United Parcel Courier provide robust
+                  solutions that will drive value for your business.
+                </p>
+                <div className='ab_button'></div>
+              </div>
+            </div>
+            <div className='card'>
+              <img
+                src='images/g2.jpg'
+                className='img-fluid'
+                alt='Card'
+              />
+              <div className='card-body w3ls-card'>
+                <h5 className='card-title'>LEAD LOGISTICS PROVIDER (LLP)</h5>
+                <p className='card-text mb-3 '>
+                  At United Parcel Courier , being LLP means instigating and
+                  managing change across your entire supply chain, to meet
+                  changing business and customer demands.
+                </p>
+                <div className='ab_button'></div>
+              </div>
+            </div>
+            <div className='card'>
+              <img
+                src='images/g3.jpg'
+                className='img-fluid'
+                alt='Card'
+              />
+              <div className='card-body w3ls-card'>
+                <h5 className='card-title'>EXPRESS DELIVERY SERVICES</h5>
+                <p className='card-text mb-3 '>
+                  When you ship with United Parcel Courier – you’re shipping
+                  with specialists in international shipping and courier
+                  delivery services! With our wide range of express parcel and
+                  package services, along with shipping and tracking solutions
+                  to fit your needs – learn how United Parcel Courier can
+                  deliver!
+                </p>
+                <div className='ab_button'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Services;
