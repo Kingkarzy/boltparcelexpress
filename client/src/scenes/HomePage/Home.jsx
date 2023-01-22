8b8eaebd9985eadf9b2833d318fc354caac6cfe0
ciijay99 <chigozieahanwa99@gmail.com>
2023-01-22 22:55:24 +0100

    commit

--- client/src/scenes/HomePage/Home.jsx
+++ client/src/scenes/HomePage/Home.jsx
@@ -12,13 +12,16 @@ const Home = () => {
 
   const handleSubmit = async (values) => {
     console.log(values);
-    const response = await fetch(`http://localhost:5001/users/${packageId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(values),
-    });
+    const response = await fetch(
+      `http://localhost:5001/users/${packageId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      }
+    );
     console.log(values);
     const data = await response.json();
     setPackageDetails(data);
@@ -45,12 +48,16 @@ const Home = () => {
               </h3>
               <h2 style={{ color: 'white' }}>Track Package</h2>
               <p>
-                NOTE: Please enter your tracking number in the box provided
-                below
+                NOTE: Please enter your tracking number in the box
+                provided below
               </p>
 
               <div
-                style={{ width: '50%', margin: 'auto', color: 'white' }}
+                style={{
+                  width: '50%',
+                  margin: 'auto',
+                  color: 'white',
+                }}
                 className='input-group-lg'
               >
                 {/* FORM  */}
@@ -79,21 +86,37 @@ const Home = () => {
                   {packageDetails && (
                     <div>
                       <p>Package Id: {packageDetails.packageId}</p>
-                      <p>Package Name: {packageDetails.packageName}</p>
-                      <p>Package Type: {packageDetails.packageType}</p>
-                      <p>Package Weight: {packageDetails.packageWeight}</p>
+                      <p>
+                        Package Name: {packageDetails.packageName}
+                      </p>
+                      <p>
+                        Package Type: {packageDetails.packageType}
+                      </p>
+                      <p>
+                        Package Weight: {packageDetails.packageWeight}
+                      </p>
                       <p>
                         Sender Name: {packageDetails.senderFirstName}{' '}
                         {packageDetails.senderLastName}
                       </p>
                       <p>
-                        Reciever Name: {packageDetails.recieverFirstName}{' '}
+                        Reciever Name:{' '}
+                        {packageDetails.recieverFirstName}{' '}
                         {packageDetails.recieverLastName}
                       </p>
-                      <p>Reciever Email: {packageDetails.recieverEmail}</p>
-                      <p>Reciever Phone: {packageDetails.recieverPhone}</p>
-                      <p>Current Status: {packageDetails.currentStatus}</p>
-                      <p>Current Location: {packageDetails.currentLocation}</p>
+                      <p>
+                        Reciever Email: {packageDetails.recieverEmail}
+                      </p>
+                      <p>
+                        Reciever Phone: {packageDetails.recieverPhone}
+                      </p>
+                      <p>
+                        Current Status: {packageDetails.currentStatus}
+                      </p>
+                      <p>
+                        Current Location:{' '}
+                        {packageDetails.currentLocation}
+                      </p>
                       <p>Time: {packageDetails.time}</p>
                     </div>
                   )}
@@ -140,8 +163,8 @@ const Home = () => {
                 </IconButton>
                 <h3 className='mt-3'>Express Shipping with UPC</h3>
                 <p className='mt-3 mb-3'>
-                  Get expert shipping advice and customs support to prepare and
-                  manage your express shipments!
+                  Get expert shipping advice and customs support to
+                  prepare and manage your express shipments!
                 </p>
               </Card>
             </div>
@@ -165,9 +188,9 @@ const Home = () => {
                 </IconButton>
                 <h3 className='mt-3'>Can We Help?</h3>
                 <p className='mt-3 mb-3'>
-                  Having trouble arranging a shipment online or tracking a
-                  package? Our UPC Express Customer Service agents are here to
-                  help you!
+                  Having trouble arranging a shipment online or
+                  tracking a package? Our UPC Express Customer Service
+                  agents are here to help you!
                 </p>
                 <div>
                   <a href='mailto:info@upcxpress.com'>
@@ -206,7 +229,9 @@ const Home = () => {
                   className='banner_form'
                 >
                   <div className='sec-left'>
-                    <label className='contact-form-text'>Email Address</label>
+                    <label className='contact-form-text'>
+                      Email Address
+                    </label>
                     <input
                       placeholder='Email Address '
                       name='email'
@@ -215,7 +240,9 @@ const Home = () => {
                     />
                   </div>
                   <div className='sec-right'>
-                    <label className='contact-form-text'>Message</label>
+                    <label className='contact-form-text'>
+                      Message
+                    </label>
                     <textarea
                       className='form-control'
                       name='message'
@@ -239,25 +266,30 @@ const Home = () => {
           <div className='inner_sec_info_wthree_agile py-5'>
             <div className='row help_full py-sm-4'>
               <div className='col-md-6 pt-lg-5 mt-lg-5 banner_bottom_left'>
-                <h4>Specialtymoves? We’re a company up to the challenge!</h4>
+                <h4>
+                  Specialtymoves? We’re a company up to the challenge!
+                </h4>
                 <p className='mb-3'>
-                  Get expert shipping/delivery advice and customs support to
-                  prepare and manage your express shipments! As an expert in
-                  international shipping and freight delivery, United Parcel
-                  Courier is a worldwide delivery express that understands the
-                  needs of small and medium businesses. United Parcel Courier is
-                  the largest consumer-to-consumer package transportation
-                  company from the United States to Europe. With over 60 years
-                  of experience handing ocean and air shipments, Sky Delivery
-                  and our operating companies have earned our reputation as a
-                  consistent, trusted, carrier that is relied upon to cost
-                  effectively handle shipments to 43 countries.
+                  Get expert shipping/delivery advice and customs
+                  support to prepare and manage your express
+                  shipments! As an expert in international shipping
+                  and freight delivery, United Parcel Courier is a
+                  worldwide delivery express that understands the
+                  needs of small and medium businesses. United Parcel
+                  Courier is the largest consumer-to-consumer package
+                  transportation company from the United States to
+                  Europe. With over 60 years of experience handing
+                  ocean and air shipments, Sky Delivery and our
+                  operating companies have earned our reputation as a
+                  consistent, trusted, carrier that is relied upon to
+                  cost effectively handle shipments to 43 countries.
                 </p>
                 <p className='mb-3'>
-                  United Parcel Courier – you’re shipping with specialists in
-                  international shipping and courier delivery services! With our
-                  wide range of express parcel and package services, along with
-                  shipping and tracking solutions to fit your needs – learn how
+                  United Parcel Courier – you’re shipping with
+                  specialists in international shipping and courier
+                  delivery services! With our wide range of express
+                  parcel and package services, along with shipping and
+                  tracking solutions to fit your needs – learn how
                   United Parcel Courier can deliver!
                 </p>
               </div>
@@ -280,10 +312,11 @@ const Home = () => {
             Express Delivery Services & International Shipping
           </h3>
           <p className='tit text-center mx-auto text-white'>
-            When you ship with UPC Express – you’re shipping with specialists in
-            international shipping and courier delivery services! With our wide
-            range of express parcel and package services, along with shipping
-            and tracking solutions to fit your needs – learn how UPC Express can
+            When you ship with UPC Express – you’re shipping with
+            specialists in international shipping and courier delivery
+            services! With our wide range of express parcel and
+            package services, along with shipping and tracking
+            solutions to fit your needs – learn how UPC Express can
             deliver!
           </p>
         </div>
@@ -294,12 +327,13 @@ const Home = () => {
             How UPC is responding to the Coronavirus
           </h3>
           <p className='tit text-center mx-auto'>
-            UPC continues to deliver worldwide. In the U.S., UPC is designated
-            among the government’s critical infrastructure and, therefore,
-            operations continue as normal. We also continue to deliver to
-            locations around the world, where permitted. As always, our highest
-            priority is to help ensure the health and safety of our employees,
-            customers, and suppliers while meeting our service commitments.
+            UPC continues to deliver worldwide. In the U.S., UPC is
+            designated among the government’s critical infrastructure
+            and, therefore, operations continue as normal. We also
+            continue to deliver to locations around the world, where
+            permitted. As always, our highest priority is to help
+            ensure the health and safety of our employees, customers,
+            and suppliers while meeting our service commitments.
           </p>
           <div className='card-deck pt-4 mt-md-4'>
             <div className='card'>
@@ -314,7 +348,8 @@ const Home = () => {
                 </h5>
                 <p className='card-text mb-3 '>
                   As an expert in international shipping, UPC Express
-                  understands the needs of small and medium businesses.
+                  understands the needs of small and medium
+                  businesses.
                 </p>
               </div>
             </div>
@@ -329,10 +364,10 @@ const Home = () => {
                   How we’re responding to Coronavirus
                 </h5>
                 <p className='card-text mb-3 '>
-                  In the wake of the Coronavirus virus, our highest priority is
-                  to do our part the help ensure the health and safety of our
-                  employees, customers, and suppliers while meeting our service
-                  commitments.
+                  In the wake of the Coronavirus virus, our highest
+                  priority is to do our part the help ensure the
+                  health and safety of our employees, customers, and
+                  suppliers while meeting our service commitments.
                 </p>
               </div>
             </div>
@@ -343,11 +378,13 @@ const Home = () => {
                 alt='Card'
               />
               <div className='card-body w3ls-card'>
-                <h5 className='card-title'>UPC Integration Solutions</h5>
+                <h5 className='card-title'>
+                  UPC Integration Solutions
+                </h5>
                 <p className='card-text mb-3 '>
-                  Whether integrating our shipping functionalities into your
-                  applications or websites, we can help you streamline processes
-                  and gain efficiencies.
+                  Whether integrating our shipping functionalities
+                  into your applications or websites, we can help you
+                  streamline processes and gain efficiencies.
                 </p>
               </div>
             </div>
@@ -359,7 +396,9 @@ const Home = () => {
         <div className='container py-4 mt-2'>
           <div className='row'>
             <div className='col-md-8 distance-agile-left'>
-              <h4 className='mt-2'>The Safest Distance Between Two Points.</h4>
+              <h4 className='mt-2'>
+                The Safest Distance Between Two Points.
+              </h4>
             </div>
             <div className='col-md-4 distance-agile-right'>
               <a href='mailto:info@upcxpress.com'> Contact Us </a>
@@ -371,10 +410,11 @@ const Home = () => {
         <div className='container py-4 mt-2'>
           <h3 className='tittle-w3ls text-center mb-3'>Our Stats</h3>
           <p className='tit text-center mx-auto'>
-            United States air hubs: Dallas, TX.; Louisville, KY. (Worldport);
-            Ontario, CA.; Philadelphia, PA.; Rockford, Il. International air
-            hubs: Cologne/Bonn, Germany; Hamilton, Ontario, Canada; Hong Kong
-            SAR, China; Shanghai, China; Shenzhen, China
+            United States air hubs: Dallas, TX.; Louisville, KY.
+            (Worldport); Ontario, CA.; Philadelphia, PA.; Rockford,
+            Il. International air hubs: Cologne/Bonn, Germany;
+            Hamilton, Ontario, Canada; Hong Kong SAR, China; Shanghai,
+            China; Shenzhen, China
           </p>
           <div className='row inner_w3l_agile_grids-1 pt-4 mt-md-4'>
             <div className='col-sm-4 w3layouts_stats_left w3_counter_grid'>
@@ -428,11 +468,12 @@ const Home = () => {
                   alt=''
                 />
                 <p>
-                  When you ship with UPC Express – you’re shipping with
-                  specialists in international shipping and courier delivery
-                  services! With our wide range of express parcel and package
-                  services, along with shipping and tracking solutions to fit
-                  your needs – learn how UPC Express can deliver!{' '}
+                  When you ship with UPC Express – you’re shipping
+                  with specialists in international shipping and
+                  courier delivery services! With our wide range of
+                  express parcel and package services, along with
+                  shipping and tracking solutions to fit your needs –
+                  learn how UPC Express can deliver!{' '}
                 </p>
               </div>
             </div>
